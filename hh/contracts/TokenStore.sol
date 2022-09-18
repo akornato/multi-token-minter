52becfd8dc5b8ca089ab457ba11f2cbb54a0bf90
Artur Kornatowski <artur.kornatowski@gmail.com>
2022-09-18 10:26:56 +0200

    add minter approvals

--- hh/contracts/TokenStore.sol
+++ hh/contracts/TokenStore.sol
@@ -6,6 +6,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 contract TokenStore is ERC1155, Ownable {
+    // Mapping from token ID to minter approvals
+    mapping(uint256 => mapping(address => bool)) private _minterApprovals;
+
+    // Mapping from token ID to boolean
+    mapping(uint256 => bool) private _tokenMinted;
+
     constructor() ERC1155("https://some.example/api/item/{id}.json") {}
 
     function mint(
@@ -14,6 +20,16 @@ contract TokenStore is ERC1155, Ownable {
         uint256 amount,
         bytes memory data
     ) external {
-        _mint(to, id, amount, data);
+        if (_tokenMinted[id]) {
+            if (_minterApprovals[id][to]) {
+                _mint(to, id, amount, data);
+            } else {
+                revert("TokenStore: Minter not approved for this token ID");
+            }
+        } else {
+            _tokenMinted[id] = true;
+            _minterApprovals[id][to] = true;
+            _mint(to, id, amount, data);
+        }
     }
 }
