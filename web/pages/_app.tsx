86b60fbb61dcdc8f9ffc579e63281ea2c6cdbc00
Artur Kornatowski <artur.kornatowski@gmail.com>
2022-09-19 07:38:26 +0200

    add useDapp

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,11 +1,24 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Mainnet, DAppProvider, Config, Goerli } from "@usedapp/core";
+import { getDefaultProvider } from "ethers";
+
+const config: Config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: getDefaultProvider("mainnet"),
+    [Goerli.chainId]: getDefaultProvider("goerli"),
+  },
+  autoConnect: true,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <DAppProvider config={config}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </DAppProvider>
   );
 }
 
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,16 +1,26 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Stack, Button } from "@chakra-ui/react";
+import { useEtherBalance, useEthers } from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
 
 const Home: NextPage = () => {
+  const { activateBrowserWallet, account } = useEthers();
+  const etherBalance = useEtherBalance(account);
   return (
     <div>
       <Head>
         <title>Multi Token Minter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Box m={4}>
-        <Button colorScheme="blue">Mint</Button>
+        <Stack direction="row">
+          <Button onClick={activateBrowserWallet}>Connect</Button>{" "}
+          <Button colorScheme="blue">Mint</Button>
+        </Stack>
+        {account && <Box mt={4}>Account: {account}</Box>}
+        {etherBalance && <Box mt={4}>Balance: {formatEther(etherBalance)} ETH</Box>}
       </Box>
     </div>
   );
