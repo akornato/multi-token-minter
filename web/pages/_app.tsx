1d1de07eeb41cd86a3a5cb2010bb1a902a8b6ca4
Artur Kornatowski <artur.kornatowski@gmail.com>
2022-09-24 22:35:38 +0200

    set chakra-ui dark theme

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Mainnet, DAppProvider, Config, Goerli, ChainId } from "@usedapp/core";
 import { getDefaultProvider } from "ethers";
+import { theme } from "web/constants/theme";
 
 const config: Config = {
   readOnlyChainId: Mainnet.chainId,
@@ -18,7 +19,7 @@ const config: Config = {
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <DAppProvider config={config}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </DAppProvider>
