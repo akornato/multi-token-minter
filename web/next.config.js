40290230653a0aa2e17526851f05276b8a7a024d
Artur Kornatowski <artur.kornatowski@gmail.com>
2022-11-02 23:07:16 +0100

    add fs webpack fix

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-}
+  // https://docs.opengsn.org/faq/troubleshooting.html#my-client-fails-with-an-error-can-t-resolve-fs
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
